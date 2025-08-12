a6719143ad87769b60e6e0eb1a9a78451cdd8afa
Matheus Couto <matheusstella.tds2.senai@gmail.com>
2025-08-12 14:59:36 -0300

    feat: create folders

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      
+      <h1>Bem-vindo ao Wiki Tech</h1>
     </div>
   );
 }
